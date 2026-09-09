2ccab97863d71604e5fffb9d317c6d991bd30b0e
Vuyi <vuyimaestro@gmail.com>
2026-09-09 23:27:48 +0200

    Intial commit - project setup with MenuItem model

---
+++ CoffeeNChill.Functions/Models/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace CoffeeNChill.Functions.Models
+{
+    public class MenuItem : ITableEntity
+    {
+        public string PartitionKey { get; set; } // Category
+        public string RowKey { get; set; }       // Item ID
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
