70e22d73d08272c0647db15d68c2e25e21ef9505
Vuyi <vuyimaestro@gmail.com>
2026-09-10 17:14:07 +0200

    Added CreateMenuItem, GetAllMenuItems and GetMenuItemsByCategory functions with Postman tests

---
+++ CoffeeNChill.Functions/Functions/GetAllMenuItems.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Azure.Data.Tables;
+using CoffeeNChill.Functions.Models;
+
+namespace CoffeeNChill.Functions.Functions
+{
+    public static class GetAllMenuItems
+    {
+        [Function("GetAllMenuItems")]
+        public static async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "menu")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var log = executionContext.GetLogger("GetAllMenuItems");
+            log.LogInformation("Processing GetAllMenuItems request...");
+
+            var serviceClient = new TableServiceClient("UseDevelopmentStorage=true");
+            var tableClient = serviceClient.GetTableClient("MenuItems");
+            await tableClient.CreateIfNotExistsAsync();
+
+            // Query all entities
+            var items = tableClient.Query<MenuItem>().ToList();
+
+            var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+            await response.WriteAsJsonAsync(items);
+            return response;
+        }
+    }
+}
---
+++ CoffeeNChill.Functions/Functions/GetMenuItemsByCategory.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Azure.Data.Tables;
+using CoffeeNChill.Functions.Models;
+
+namespace CoffeeNChill.Functions.Functions
+{
+    public static class GetMenuItemsByCategory
+    {
+        [Function("GetMenuItemsByCategory")]
+        public static async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "menu/category/{category}")] HttpRequestData req,
+            string category,
+            FunctionContext executionContext)
+        {
+            var log = executionContext.GetLogger("GetMenuItemsByCategory");
+            log.LogInformation($"Processing GetMenuItemsByCategory request for category: {category}");
+
+            var serviceClient = new TableServiceClient("UseDevelopmentStorage=true");
+            var tableClient = serviceClient.GetTableClient("MenuItems");
+            await tableClient.CreateIfNotExistsAsync();
+
+            // Query by PartitionKey (category)
+            var items = tableClient.Query<MenuItem>(x => x.PartitionKey == category).ToList();
+
+            var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+            await response.WriteAsJsonAsync(items);
+            return response;
+        }
+    }
+}
